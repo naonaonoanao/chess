4844b1fc5c9f8c94c2ae25b296beb1ef04389236
nao <egorovanasta226@gmail.com>
2024-01-13 17:30:27 +0700

    feat(reg): добавлены всплывающие окна ошибок
    добавлены всплывающие окна ошибок для всех строк ввода

--- chess/RegistrationView.xaml.cs
+++ chess/RegistrationView.xaml.cs
@@ -22,7 +22,14 @@ namespace uwp
         public RegistrationView()
         {
             InitializeComponent();
+            RegUsernameTextBox.TextChanged += UsernameTextBox_TextChanged;
         }
+
+        private const int МАКСИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ПАРОЛЯ = 20;
+        private const int МИНИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ПАРОЛЯ = 12;
+
+        private const int МАКСИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ЛОГИНА = 25;
+        private const int МИНИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ЛОГИНА = 12;
 
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
@@ -35,6 +42,31 @@ namespace uwp
             {
                 loginTextBlock.Visibility = Visibility.Collapsed;
             }
+            // Проверка ограничения по количеству символов
+            if (RegPasswordTextBox.Password.Length > МАКСИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ПАРОЛЯ)
+            {
+                ShowErrorPopupPassword("Слишком длинный пароль");
+            }
+            else if (RegPasswordTextBox.Password.Length < МИНИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ПАРОЛЯ)
+            {
+                ShowErrorPopupPassword("Слишком короткий пароль");
+            }
+            else
+            {
+                HideErrorPopupPassword();
+            }
+        }
+
+        private void ShowErrorPopupPassword(string errorMessage)
+        {
+            ErrorMessage.Text = errorMessage;
+            ErrorPopupPassword.IsOpen = true;
+        }
+
+        private void HideErrorPopupPassword()
+        {
+            ErrorMessage.Text = string.Empty;
+            ErrorPopupPassword.IsOpen = false;
         }
 
         private void RepeatPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
@@ -47,6 +79,51 @@ namespace uwp
             else
             {
                 loginTextBlock.Visibility = Visibility.Collapsed;
+            }
+            // Проверка ограничения по количеству символов
+            if (RegRepeatPasswordTextBox.Password.Length > МАКСИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ПАРОЛЯ)
+            {
+                ShowErrorPopupPasswordRepeat("Слишком длинный пароль");
+            }
+
+            else if (RegRepeatPasswordTextBox.Password.Length < МИНИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ПАРОЛЯ)
+            {
+                ShowErrorPopupPasswordRepeat("Слишком короткий пароль");
+            }
+            else
+            {
+                HideErrorPopupPasswordRepeat();
+            }
+        }
+
+        private void ShowErrorPopupPasswordRepeat(string errorMessage)
+        {
+            ErrorMessage.Text = errorMessage;
+            ErrorPopupPasswordRepeat.IsOpen = true;
+        }
+
+        private void HideErrorPopupPasswordRepeat()
+        {
+            ErrorMessage.Text = string.Empty;
+            ErrorPopupPasswordRepeat.IsOpen = false;
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox RegUsernameTextBox = sender as TextBox;
+            // Проверка ограничения по количеству символов
+            if (RegUsernameTextBox.Text.Length > МАКСИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ЛОГИНА)
+            {
+                ShowErrorPopupLogin("Слишком длинный логин");
+            }
+
+            else if (RegRepeatPasswordTextBox.Password.Length < МИНИМАЛЬНОЕ_КОЛИЧЕСТВО_СИМВОЛОВ_ЛОГИНА)
+            {
+                ShowErrorPopupLogin("Слишком короткий логин");
+            }
+            else
+            {
+                HideErrorPopupLogin();
             }
         }
 
@@ -64,6 +141,18 @@ namespace uwp
             WindowEventArgs args = new WindowEventArgs(windowName);
 
             RequestChangeContent?.Invoke(this, args);
+        }
+
+        private void ShowErrorPopupLogin(string errorMessage)
+        {
+            ErrorMessage.Text = errorMessage;
+            ErrorPopupLogin.IsOpen = true;
+        }
+
+        private void HideErrorPopupLogin()
+        {
+            ErrorMessage.Text = string.Empty;
+            ErrorPopupLogin.IsOpen = false;
         }
 
         private void RegButton_Click(object sender, RoutedEventArgs e)
@@ -107,7 +196,7 @@ namespace uwp
 
             if (!(bool)conditionsBox.IsChecked)
             {
-                ErrorMessage.Text = "Подведтите, что вы прочитали соглашение!";
+                ErrorMessage.Text = "Подтвердите, что вы прочитали соглашение!";
                 ErrorMessage.Visibility = Visibility.Visible;
 
                 return;
